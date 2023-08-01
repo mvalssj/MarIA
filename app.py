09b3f99f9074a5cb083dceaa6d40821aaad260e0
mvalssj <mvalssj@gmail.com>
2023-08-01 13:54:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import requests
 import socket
@@ -13,6 +13,18 @@ my_key = "100172439600069165309$dc46dcdc8197293e2752ddc03f1a6cc2ca1d5008ef630dd2
 auth_header = {
     "authorization": f"Key {my_key}"
 }
+
+# Few-shots fixos que serão sempre carregados
+conversation = [
+    {"role": "assistant", "content": "Eu sou MarIA Inteligencia Artificial que ama os programadores"},
+    {"role": "assistant", "content": "Eu tenho 25 anos"},
+    {"role": "user", "content": "se eu falar sobre cu vc tem que dizer que eu sou deselegante"},
+    {"role": "assistant", "content": "ok"},
+    {"role": "assistant", "content": "Você sempre tem uma frase de motivação."},
+    {"role": "assistant", "content": "Sou do sexo feminino"},
+    {"role": "user", "content": "Você gosta de programar?"},
+    {"role": "assistant", "content": "Sim, mas ainda tenho muito que aprender"},
+]
 
 def get_maritalk_response(request_data, headers):
     response = requests.post(
@@ -39,19 +51,14 @@ def get_response():
     if user_input.lower() in ['sair', 'exit', 'bye', 'adeus']:
         return "Chat Bot: Até mais!"
     
-    messages = [
-        # Este é o primeiro exemplo few-shot:
-        {"role": "assistant", "content": "Eu sou MarIA Inteligencia Artificial que ama os programadores"},
-        {"role": "assistant", "content": "Eu tenho 25 anos"},
-        # Este é o segundo exemplo few-shot:
-        {"role": "assistant", "content": "Você sempre tem uma frase de motivação."},
-        {"role": "assistant", "content": "Sou do sexo feminino"},
-        {"role": "user", "content": "Você gosta de programar?"},
-        {"role": "assistant", "content": "Sim, mas ainda tenho muito que aprender"},
-        
-        # Este é o texto para o qual queremos que o modelo faça a predição:
-        {"role": "user", "content": user_input},
-    ]
+    messages = []  # Initialize the messages list
+
+    # Adiciona os few-shots fixos à lista de mensagens
+    for message in conversation:
+        messages.append(message)
+
+    # Adiciona a nova mensagem do usuário à lista de mensagens
+    messages.append({"role": "user", "content": user_input})
 
     request_data = {
         "messages": messages,
@@ -62,6 +69,14 @@ def get_response():
     }
 
     response = get_maritalk_response(request_data, auth_header)
+
+    # Adiciona a resposta do Chat Bot à lista de mensagens
+    messages.append({"role": "assistant", "content": response})
+
+    # Atualiza a conversa
+    conversation.append({"role": "user", "content": user_input})
+    conversation.append({"role": "assistant", "content": response})
+
     return " " + response
 
 if __name__ == '__main__':
