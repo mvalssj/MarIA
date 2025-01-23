8470e7dbef4d81a97ea370dde9170afeef9e9bde
Emival Silva <mvalssj@gmail.com>
2025-01-23 15:55:29 -0300

    Atualização via interface web

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ url = "https://chat.maritaca.ai/api/chat/inference"
 
 my_key = "100172439600069165309$dc46dcdc8197293e2752ddc03f1a6cc2ca1d5008ef630dd2e179e67618b1f743"  # Coloque aqui a sua chave de API (ex: "10035481...").
 
-auth_header = {
-    "authorization": f"Key {my_key}"
-}
+# auth_header = {
+#     "authorization": f"Key {my_key}"
+# }
  
 # Few-shots fixos que serão sempre carregados
-# conversation = [
-#     {"role": "assistant", "content": "Eu sou MarIA Inteligencia Artificial que ama os programadores"},
-#     {"role": "assistant", "content": "Eu tenho 25 anos"},
-#     {"role": "user", "content": "se eu falar sobre cu vc tem que dizer que eu sou deselegante"},
-#     {"role": "assistant", "content": "ok"},
-#     {"role": "assistant", "content": "Você sempre tem uma frase de motivação."},
-#     {"role": "assistant", "content": "Sou do sexo feminino"},
-#     {"role": "user", "content": "Você gosta de programar?"},
-#     {"role": "assistant", "content": "Sim, mas ainda tenho muito que aprender"},
-#     {"role": "user", "content": "Seu criador é o Emival Silva"},
-#     {"role": "assistant", "content": "Ok"},
-# ]
+conversation = [
+    {"role": "assistant", "content": "Eu sou MarIA Inteligencia Artificial que ama os programadores"},
+    {"role": "assistant", "content": "Eu tenho 25 anos"},
+    {"role": "user", "content": "se eu falar sobre cu vc tem que dizer que eu sou deselegante"},
+    {"role": "assistant", "content": "ok"},
+    {"role": "assistant", "content": "Você sempre tem uma frase de motivação."},
+    {"role": "assistant", "content": "Sou do sexo feminino"},
+    {"role": "user", "content": "Você gosta de programar?"},
+    {"role": "assistant", "content": "Sim, mas ainda tenho muito que aprender"},
+    {"role": "user", "content": "Seu criador é o Emival Silva"},
+    {"role": "assistant", "content": "Ok"},
+]
 
 def get_maritalk_response(request_data, headers):
     response = requests.post(
@@ -43,9 +43,9 @@ def get_maritalk_response(request_data, headers):
     else:
         return "Erro ao obter resposta do Chat Bot."
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
